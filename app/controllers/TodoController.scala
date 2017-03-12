f8339d7c7d768e724d43378b99aa7528deb2618b
pluselc <user@user-no-MacBook-Pro.local>
2017-03-13 03:38:54 +0900

    create list template

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -4,9 +4,15 @@ import javax.inject.Inject
 
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
+import views._
 
 class TodoController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
   def helloWorld() = Action { implicit request =>
     Ok("Hello World")
+  }
+
+  def list() = Action { implicit request =>
+    val message: String = "ここにリストを表示"
+    Ok(html.list(message))
   }
 }
